c68090ec67201a2e7e4459782786a7f2ae7d287f
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-03-07 15:36:59 +0300

    Add publisher model

---
+++ src/main/java/ru/example/gameslogmanager/models/Publisher.java
@@ -0,0 +1,49 @@
+package ru.example.gameslogmanager.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "publisher")
+public class Publisher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "publisher_id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    public Publisher() {
+    }
+
+    public Publisher(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
