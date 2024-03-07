23927b5c266798d7a804defb12c4ee7883de98bf
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-03-07 15:36:06 +0300

    Initial commit

---
+++ src/main/java/ru/example/gameslogmanager/GamesLogManagerApplication.java
@@ -0,0 +1,13 @@
+package ru.example.gameslogmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesLogManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamesLogManagerApplication.class, args);
+    }
+
+}
