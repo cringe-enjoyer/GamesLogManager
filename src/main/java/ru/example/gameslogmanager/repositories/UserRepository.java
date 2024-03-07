5a61807c21528a52ab58ae5926d76943f8845e2a
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-03-07 15:38:31 +0300

    Add User repository model

---
+++ src/main/java/ru/example/gameslogmanager/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package ru.example.gameslogmanager.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.example.gameslogmanager.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
