01ff1ed96e0f0b5ef89567ba3f3fca5b763af2e5
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-03-07 15:36:23 +0300

    Add user model

---
+++ src/main/java/ru/example/gameslogmanager/models/User.java
@@ -0,0 +1,86 @@
+package ru.example.gameslogmanager.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private int id;
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "steam_id")
+    private String steamId;
+
+    @Column(name = "nickname")
+    private String nickname;
+
+    public User() {
+    }
+
+    public User(String login, String password, String email, String steamId, String nickname) {
+        this.login = login;
+        this.password = password;
+        this.email = email;
+        this.steamId = steamId;
+        this.nickname = nickname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSteamId() {
+        return steamId;
+    }
+
+    public void setSteamId(String steamId) {
+        this.steamId = steamId;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+}
