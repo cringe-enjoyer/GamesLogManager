4514444482cdd25fcdd8224689128d4a0b9c3e15
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-03-09 16:34:14 +0300

    Add Game repository

---
+++ src/main/java/ru/example/gameslogmanager/repositories/GameRepository.java
@@ -0,0 +1,20 @@
+package ru.example.gameslogmanager.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.example.gameslogmanager.models.Developer;
+import ru.example.gameslogmanager.models.Game;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface GameRepository extends JpaRepository<Game, Integer> {
+
+    Optional<Game> findByTitle(String title);
+
+    List<Game> findAllByGenre(String genre);
+
+    List<Game> findAllByDeveloper(Developer developer);
+
+}
