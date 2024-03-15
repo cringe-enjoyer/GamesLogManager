4fbfa6a3aaa219a54b9f7d1279f8b025b57a6aaa
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-03-15 15:06:38 +0300

    Add DTO for games list

---
+++ src/main/java/ru/example/gameslogmanager/dto/GamesListDTO.java
@@ -0,0 +1,55 @@
+package ru.example.gameslogmanager.dto;
+
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * DTO for {@link ru.example.gameslogmanager.models.GamesList}
+ */
+public class GamesListDTO implements Serializable {
+    private String name;
+    private UserDTO user;
+    private List<UsersGameDTO> usersGamesList;
+    private long gamesCount;
+
+    public GamesListDTO(String name, UserDTO user) {
+        this.name = name;
+        this.user = user;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public UserDTO getUser() {
+        return user;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUser(UserDTO user) {
+        this.user = user;
+    }
+
+    public List<UsersGameDTO> getUsersGamesList() {
+        return usersGamesList;
+    }
+
+    public void setUsersGamesList(List<UsersGameDTO> usersGamesList) {
+        this.usersGamesList = usersGamesList;
+    }
+
+    public long getGamesCount() {
+        return usersGamesList.size();
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "(" +
+                "name = " + name + ", " +
+                "user = " + user + ")";
+    }
+}
