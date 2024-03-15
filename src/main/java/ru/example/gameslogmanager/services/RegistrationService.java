38ad46e110ea7ba10b9933cbd8f97d9cf330a1fe
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-03-15 15:37:38 +0300

    Add registration service

---
+++ src/main/java/ru/example/gameslogmanager/services/RegistrationService.java
@@ -0,0 +1,27 @@
+package ru.example.gameslogmanager.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.example.gameslogmanager.models.User;
+import ru.example.gameslogmanager.repositories.UserRepository;
+
+@Service
+public class RegistrationService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public RegistrationService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    public void register(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole("USER");
+        userRepository.save(user);
+    }
+}
