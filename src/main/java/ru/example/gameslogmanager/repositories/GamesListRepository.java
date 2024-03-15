c8714e49dec3bf2c8234e3dfba82a2f85a3faea4
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-03-15 15:13:14 +0300

    Add methods for games count by list id and find all lists by user

--- src/main/java/ru/example/gameslogmanager/repositories/GamesListRepository.java
+++ src/main/java/ru/example/gameslogmanager/repositories/GamesListRepository.java
@@ -3,7 +3,13 @@ package ru.example.gameslogmanager.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.example.gameslogmanager.models.GamesList;
+import ru.example.gameslogmanager.models.User;
+
+import java.util.List;
 
 @Repository
 public interface GamesListRepository extends JpaRepository<GamesList, Integer> {
+    List<GamesList> findAllByUser(User user);
+
+    Long countById(int id);
 }
