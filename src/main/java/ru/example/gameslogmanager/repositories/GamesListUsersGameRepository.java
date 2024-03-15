46f2f58df53e33d5fc4506e9d3c03b49043cdfba
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-03-15 15:14:00 +0300

    Delete table GamesListUsersGame

--- src/main/java/ru/example/gameslogmanager/repositories/GamesListUsersGameRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.example.gameslogmanager.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.example.gameslogmanager.models.GamesListUsersGame;
-
-@Repository
-public interface GamesListUsersGameRepository extends JpaRepository<GamesListUsersGame, Integer> {
-}
