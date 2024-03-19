97d026813343964ea1adc8f91ff1c748e8071a21
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-03-19 16:51:52 +0300

    Add save and getUsersGameByGameAndList methods

--- src/main/java/ru/example/gameslogmanager/repositories/UsersGameRepository.java
+++ src/main/java/ru/example/gameslogmanager/repositories/UsersGameRepository.java
@@ -2,8 +2,13 @@ package ru.example.gameslogmanager.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ru.example.gameslogmanager.models.Game;
+import ru.example.gameslogmanager.models.GamesList;
 import ru.example.gameslogmanager.models.UsersGame;
+
+import java.util.Optional;
 
 @Repository
 public interface UsersGameRepository extends JpaRepository<UsersGame, Integer> {
+    Optional<UsersGame> findByGameAndList(Game game, GamesList list);
 }
--- src/main/java/ru/example/gameslogmanager/services/UsersGameService.java
+++ src/main/java/ru/example/gameslogmanager/services/UsersGameService.java
@@ -3,6 +3,8 @@ package ru.example.gameslogmanager.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.example.gameslogmanager.models.Game;
+import ru.example.gameslogmanager.models.GamesList;
 import ru.example.gameslogmanager.models.UsersGame;
 import ru.example.gameslogmanager.repositories.UsersGameRepository;
 
@@ -23,5 +25,12 @@ public class UsersGameService {
         return usersGameRepository.findById(id);
     }
 
+    public Optional<UsersGame> getUsersGameByGameAndList(Game game, GamesList list) {
+        return usersGameRepository.findByGameAndList(game, list);
+    }
+
+    public void save(UsersGame game) {
+        usersGameRepository.save(game);
+    }
 
 }
