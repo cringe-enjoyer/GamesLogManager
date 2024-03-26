06438e0ffb9952c6837ca862bd9d9f4d37701d06
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-03-26 14:21:42 +0300

    Refactor UsersGameDTO

--- src/main/java/ru/example/gameslogmanager/dto/UsersGameDTO.java
+++ src/main/java/ru/example/gameslogmanager/dto/UsersGameDTO.java
@@ -1,53 +1,114 @@
 package ru.example.gameslogmanager.dto;
 
+import com.lukaspradel.steamapi.data.json.playerachievements.Achievement;
+
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * DTO for {@link ru.example.gameslogmanager.models.UsersGame}
  */
 public class UsersGameDTO implements Serializable {
-    private final int userRating;
-    private final LocalDateTime userTime;
-    private final String userComment;
-    private final String userNote;
-    private final GameDTO game;
+    private int userRating;
+    private LocalDateTime userTime;
+    private String userComment;
+    private String userNote;
+    private GameDTO game;
+    private GamesListDTO list;
+    private LocalDate dateAdded;
+    private List<Achievement> achievements;
 
-    public UsersGameDTO(int userRating, LocalDateTime userTime, String userComment, String userNote, GameDTO game) {
+
+    public UsersGameDTO() {
+    }
+
+    public UsersGameDTO(int userRating, LocalDateTime userTime, String userComment, String userNote, GameDTO game,
+                        GamesListDTO list, LocalDate dateAdded) {
         this.userRating = userRating;
         this.userTime = userTime;
         this.userComment = userComment;
         this.userNote = userNote;
         this.game = game;
+        this.list = list;
+        this.dateAdded = dateAdded;
     }
 
     public int getUserRating() {
         return userRating;
     }
 
+    public void setUserRating(int userRating) {
+        this.userRating = userRating;
+    }
+
     public LocalDateTime getUserTime() {
         return userTime;
+    }
+
+    public void setUserTime(LocalDateTime userTime) {
+        this.userTime = userTime;
     }
 
     public String getUserComment() {
         return userComment;
     }
 
+    public void setUserComment(String userComment) {
+        this.userComment = userComment;
+    }
+
     public String getUserNote() {
         return userNote;
+    }
+
+    public void setUserNote(String userNote) {
+        this.userNote = userNote;
     }
 
     public GameDTO getGame() {
         return game;
     }
 
+    public void setGame(GameDTO game) {
+        this.game = game;
+    }
+
+    public GamesListDTO getList() {
+        return list;
+    }
+
+    public void setList(GamesListDTO list) {
+        this.list = list;
+    }
+
+    public LocalDate getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(LocalDate dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+    public List<Achievement> getAchievements() {
+        return achievements;
+    }
+
+    public void setAchievements(List<Achievement> achievements) {
+        this.achievements = achievements;
+    }
+
     @Override
     public String toString() {
-        return getClass().getSimpleName() + "(" +
-                "userRating = " + userRating + ", " +
-                "userTime = " + userTime + ", " +
-                "userComment = " + userComment + ", " +
-                "userNote = " + userNote + ", " +
-                "game = " + game + ")";
+        return "UsersGameDTO{" +
+                "userRating=" + userRating +
+                ", userTime=" + userTime +
+                ", userComment='" + userComment + '\'' +
+                ", userNote='" + userNote + '\'' +
+                ", game=" + game +
+                ", list=" + list +
+                ", dateAdded=" + dateAdded +
+                '}';
     }
 }
