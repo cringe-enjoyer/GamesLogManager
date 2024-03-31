2c26c99b4b4db7b60f15d0ccb2a05d407932eb4e
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-03-31 12:47:08 +0300

    Add login request

---
+++ src/main/java/ru/example/gameslogmanager/dto/LoginRequest.java
@@ -0,0 +1,33 @@
+package ru.example.gameslogmanager.dto;
+
+/**
+ * Запрос от пользователя содержащий данные для входа
+ */
+public class LoginRequest {
+    private String login;
+    private String password;
+
+    public LoginRequest() {
+    }
+
+    public LoginRequest(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
