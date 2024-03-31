7a3090559105e120b06e8f73fd2e76442cc9a4db
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-03-31 13:02:46 +0300

    Refactor auth controller for jwt token

--- src/main/java/ru/example/gameslogmanager/controllers/AuthController.java
+++ src/main/java/ru/example/gameslogmanager/controllers/AuthController.java
@@ -1,10 +1,13 @@
 package ru.example.gameslogmanager.controllers;
 
+import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.example.gameslogmanager.dto.LoginRequest;
+import ru.example.gameslogmanager.dto.LoginResponse;
 import ru.example.gameslogmanager.dto.UserDTO;
 import ru.example.gameslogmanager.models.User;
 import ru.example.gameslogmanager.services.RegistrationService;
@@ -15,6 +18,7 @@ public class AuthController {
     private final RegistrationService registrationService;
     private final ModelMapper modelMapper;
 
+
     @Autowired
     public AuthController(RegistrationService registrationService, ModelMapper modelMapper) {
         this.registrationService = registrationService;
@@ -22,10 +26,16 @@ public class AuthController {
     }
 
     @PostMapping("/registration")
-    public ResponseEntity<HttpStatus> performRegistration(@RequestBody UserDTO userDTO) {
+    public ResponseEntity<HttpStatus> performRegistration(@RequestBody @Valid UserDTO userDTO) {
+        //TODO: Добавить создание 3 списков
         User user = convertToUser(userDTO);
         registrationService.register(user);
         return ResponseEntity.ok(HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public LoginResponse login(@RequestBody LoginRequest request) {
+        return registrationService.login(request.getLogin(), request.getPassword());
     }
 
     private User convertToUser(UserDTO userDTO) {
