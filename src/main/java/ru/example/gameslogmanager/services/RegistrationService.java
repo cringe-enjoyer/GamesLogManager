8985171f9f1388ed1b439c5e45a942d2592ed504
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-03-31 13:00:44 +0300

    Refactor RegistrationService for jwt

--- src/main/java/ru/example/gameslogmanager/services/RegistrationService.java
+++ src/main/java/ru/example/gameslogmanager/services/RegistrationService.java
@@ -1,22 +1,37 @@
 package ru.example.gameslogmanager.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.example.gameslogmanager.dto.LoginResponse;
 import ru.example.gameslogmanager.models.User;
 import ru.example.gameslogmanager.repositories.UserRepository;
+import ru.example.gameslogmanager.security.JWTIssuer;
+import ru.example.gameslogmanager.security.UserPrincipal;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
 public class RegistrationService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JWTIssuer jwtIssuer;
+    private final AuthenticationManager authenticationManager;
 
     @Autowired
-    public RegistrationService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public RegistrationService(UserRepository userRepository, PasswordEncoder passwordEncoder, JWTIssuer jwtIssuer,
+                               AuthenticationManager authenticationManager) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.jwtIssuer = jwtIssuer;
+        this.authenticationManager = authenticationManager;
     }
 
     @Transactional
@@ -24,5 +39,17 @@ public class RegistrationService {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setRole("USER");
         userRepository.save(user);
+    }
+
+    public LoginResponse login(String login, String password) {
+        Authentication authenticate = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(login, password));
+
+        SecurityContextHolder.getContext().setAuthentication(authenticate);
+        UserPrincipal principal = (UserPrincipal) authenticate.getPrincipal();
+        List<String> roles = principal.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList();
+        String token = jwtIssuer.issue(principal.getId(), principal.getLogin(), roles);
+
+        return new LoginResponse(token);
     }
 }
