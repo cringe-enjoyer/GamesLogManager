4de20c7303b1cd3e6107b0f9e4f67e5e1d67c0ff
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-04-08 16:44:32 +0300

    Add methods for user stats by platforms and genres

--- src/main/java/ru/example/gameslogmanager/services/StatisticService.java
+++ src/main/java/ru/example/gameslogmanager/services/StatisticService.java
@@ -3,10 +3,11 @@ package ru.example.gameslogmanager.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import ru.example.gameslogmanager.models.GamesList;
-import ru.example.gameslogmanager.models.User;
-import ru.example.gameslogmanager.models.UsersGoal;
+import ru.example.gameslogmanager.models.*;
+import ru.example.gameslogmanager.utils.DefaultLists;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -20,18 +21,24 @@ import java.util.Optional;
 public class StatisticService {
 
     private final UsersGameService usersGameService;
+
     private final GamesListService gamesListService;
 
-    private final String finished = "пройдены";
     private final UsersGoalService usersGoalService;
+
+    private final PlatformService platformService;
+
+    private final GenreService genreService;
 
 
     @Autowired
     public StatisticService(UsersGameService usersGameService, GamesListService gamesListService,
-                            UsersGoalService usersGoalService) {
+                            UsersGoalService usersGoalService, PlatformService platformService, GenreService genreService) {
         this.usersGameService = usersGameService;
         this.gamesListService = gamesListService;
         this.usersGoalService = usersGoalService;
+        this.platformService = platformService;
+        this.genreService = genreService;
     }
 
     /**
@@ -41,7 +48,7 @@ public class StatisticService {
      */
     // TODO: Доработать
     public Map<String, Object> getUserStats(User user) {
-        Optional<GamesList> gamesList = gamesListService.findByUserAndName(user, finished);
+        Optional<GamesList> gamesList = gamesListService.getByUserAndName(user, DefaultLists.FINISHED.getRuValue());
 
         Integer gamesFinished = 0;
 
@@ -51,15 +58,41 @@ public class StatisticService {
         Map<String, Object> result = new HashMap<>();
         result.put("games-finished", gamesFinished);
 
-        List<GamesList> userLists = gamesListService.findAllByUser(user);
+        List<GamesList> userLists = gamesListService.getAllByUser(user);
         result.put("users-lists", userLists);
         //userLists.forEach(list -> result.put(list.getName() + "-list", list.getUsersGames().size()));
 
-        List<UsersGoal> userGoal = usersGoalService.findGoalsByUser(user);
+        List<UsersGoal> userGoal = usersGoalService.getGoalsByUser(user);
 
         if (!userGoal.isEmpty())
             result.put("user-goal", userGoal);
 
         return result;
+    }
+
+
+    public Map<Platform, List<UsersGame>> getGamesByPlatforms(User user) {
+        Map<Platform, List<UsersGame>> gamesByPlatforms = new HashMap<>();
+        List<Platform> allPlatforms = platformService.getAllPlatforms();
+
+        for (Platform platform : allPlatforms) {
+            //TODO: Проверить этот запрос
+            List<UsersGame> usersGamesByPlatform = usersGameService.getUsersGamesByPlatformAndUser(platform, user);
+            gamesByPlatforms.put(platform, usersGamesByPlatform);
+        }
+
+        return gamesByPlatforms;
+    }
+
+    /**
+     * Возвращает Map содержащую список игр соответствующий жанру
+     * @param user пользователь чьи игры будут возвращены
+     * @return Map с ключами жанрами и списками игр
+     */
+    public Map<Genre, List<UsersGame>> getGamesByGenres(User user) {
+        Map<Genre, List<UsersGame>> gamesByGenres = new HashMap<>();
+        List<Genre> genres = genreService.getAllGenres();
+        genres.forEach(genre -> gamesByGenres.put(genre, usersGameService.getUsersGamesByGenre(genre, user)));
+        return gamesByGenres;
     }
 }
