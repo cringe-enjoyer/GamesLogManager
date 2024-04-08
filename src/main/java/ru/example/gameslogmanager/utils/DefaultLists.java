5bb1dd334af4505d234464c318b7778d43bcdcb8
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-04-08 15:24:22 +0300

    Add enum with default lists names

---
+++ src/main/java/ru/example/gameslogmanager/utils/DefaultLists.java
@@ -0,0 +1,23 @@
+package ru.example.gameslogmanager.utils;
+
+/**
+ * Перечисление содержащие названия стандартных списков в БД
+ */
+public enum DefaultLists {
+    FINISHED("пройдено"),
+    BACKLOG("бэклог"),
+    DROP("брошено");
+
+    /**
+     * Название на русском в БД
+     */
+    final String ruValue;
+
+    DefaultLists(String ruValue) {
+        this.ruValue = ruValue;
+    }
+
+    public String getRuValue() {
+        return ruValue;
+    }
+}
