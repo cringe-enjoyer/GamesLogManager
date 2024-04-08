28785f3ceb8a402ea35673a0c48df6f8b7e21841
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-04-08 16:01:04 +0300

    Add methods equals and hashCode for GamesListDTO

--- src/main/java/ru/example/gameslogmanager/dto/GamesListDTO.java
+++ src/main/java/ru/example/gameslogmanager/dto/GamesListDTO.java
@@ -3,6 +3,7 @@ package ru.example.gameslogmanager.dto;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * DTO for {@link ru.example.gameslogmanager.models.GamesList}
@@ -54,5 +55,27 @@ public class GamesListDTO implements Serializable {
         return getClass().getSimpleName() + "(" +
                 "name = " + name + ", " +
                 "user = " + user + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GamesListDTO that = (GamesListDTO) o;
+
+        if (gamesCount != that.gamesCount) return false;
+        if (!Objects.equals(name, that.name)) return false;
+        if (!Objects.equals(user, that.user)) return false;
+        return Objects.equals(usersGamesList, that.usersGamesList);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (user != null ? user.hashCode() : 0);
+        result = 31 * result + (usersGamesList != null ? usersGamesList.hashCode() : 0);
+        result = 31 * result + (int) (gamesCount ^ (gamesCount >>> 32));
+        return result;
     }
 }
