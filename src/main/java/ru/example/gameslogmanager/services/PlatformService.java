a7113d03a32291d55eb7be15b0945187370eff50
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-04-08 15:31:13 +0300

    Add service for game platform

---
+++ src/main/java/ru/example/gameslogmanager/services/PlatformService.java
@@ -0,0 +1,25 @@
+package ru.example.gameslogmanager.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.example.gameslogmanager.models.Platform;
+import ru.example.gameslogmanager.repositories.PlatformRepository;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class PlatformService {
+
+    private final PlatformRepository platformRepository;
+
+    @Autowired
+    public PlatformService(PlatformRepository platformRepository) {
+        this.platformRepository = platformRepository;
+    }
+
+    public List<Platform> getAllPlatforms() {
+        return platformRepository.findAll();
+    }
+}
