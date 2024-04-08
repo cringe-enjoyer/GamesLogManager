932ce787e46196be6016df7207db56161d3ca7d1
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-04-08 15:26:31 +0300

    Add fields smallImgUrl, backgroundImgUrl, verified and realiseDate to Game model

--- src/main/java/ru/example/gameslogmanager/models/Game.java
+++ src/main/java/ru/example/gameslogmanager/models/Game.java
@@ -2,7 +2,8 @@ package ru.example.gameslogmanager.models;
 
 import jakarta.persistence.*;
 
-import java.util.LinkedHashSet;
+import java.time.LocalDate;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -28,11 +29,23 @@ public class Game {
     @Column(name = "short_description", length = Integer.MAX_VALUE)
     private String shortDescription;
 
-    @Column(name = "image", length = Integer.MAX_VALUE)
-    private String image;
+    @Column(name = "image_url", length = Integer.MAX_VALUE)
+    private String imageUrl;
 
     @Column(name = "steam_id")
     private Integer steamId;
+
+    @Column(name = "small_img_url", length = Integer.MAX_VALUE)
+    private String smallImgUrl;
+
+    @Column(name = "background_img_url")
+    private String backgroundImgUrl;
+
+    @Column(name = "verified")
+    private Boolean verified;
+
+    @Column(name = "realise_date")
+    private LocalDate realiseDate;
 
     //TODO: Протестировать сохранение
     @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
@@ -61,14 +74,14 @@ public class Game {
         this.title = title;
     }
 
-    public Game(String title, String description, double rating, double avgTime, String shortDescription, String image,
+    public Game(String title, String description, double rating, double avgTime, String shortDescription, String imageUrl,
                 Integer steamId, Set<Developer> developers, Set<Publisher> publishers, Set<Genre> genres) {
         this.title = title;
         this.description = description;
         this.rating = rating;
         this.avgTime = avgTime;
         this.shortDescription = shortDescription;
-        this.image = image;
+        this.imageUrl = imageUrl;
         this.steamId = steamId;
         this.developers = developers;
         this.publishers = publishers;
@@ -124,12 +137,12 @@ public class Game {
         this.steamId = steamId;
     }
 
-    public String getImage() {
-        return image;
+    public String getImageUrl() {
+        return imageUrl;
     }
 
-    public void setImage(String image) {
-        this.image = image;
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
     }
 
     public String getShortDescription() {
@@ -138,6 +151,38 @@ public class Game {
 
     public void setShortDescription(String shortDescription) {
         this.shortDescription = shortDescription;
+    }
+
+    public LocalDate getRealiseDate() {
+        return realiseDate;
+    }
+
+    public void setRealiseDate(LocalDate realiseDate) {
+        this.realiseDate = realiseDate;
+    }
+
+    public Boolean getVerified() {
+        return verified;
+    }
+
+    public void setVerified(Boolean verified) {
+        this.verified = verified;
+    }
+
+    public String getSmallImgUrl() {
+        return smallImgUrl;
+    }
+
+    public void setSmallImgUrl(String smallImgUrl) {
+        this.smallImgUrl = smallImgUrl;
+    }
+
+    public String getBackgroundImgUrl() {
+        return backgroundImgUrl;
+    }
+
+    public void setBackgroundImgUrl(String backgroundImgUrl) {
+        this.backgroundImgUrl = backgroundImgUrl;
     }
 
     public Set<Genre> getGenres() {
@@ -173,11 +218,44 @@ public class Game {
                 ", rating=" + rating +
                 ", avgTime=" + avgTime +
                 ", shortDescription='" + shortDescription + '\'' +
-                ", image='" + image + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
                 ", steamId=" + steamId +
+                ", smallImgUrl='" + smallImgUrl + '\'' +
+                ", backgroundImgUrl='" + backgroundImgUrl + '\'' +
+                ", verified=" + verified +
+                ", realiseDate=" + realiseDate +
                 ", developers=" + developers +
                 ", publishers=" + publishers +
                 ", genres=" + genres +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Game game = (Game) o;
+        return id == game.id && Double.compare(rating, game.rating) == 0 && Double.compare(avgTime, game.avgTime) == 0 && Objects.equals(title, game.title) && Objects.equals(description, game.description) && Objects.equals(shortDescription, game.shortDescription) && Objects.equals(imageUrl, game.imageUrl) && Objects.equals(steamId, game.steamId) && Objects.equals(smallImgUrl, game.smallImgUrl) && Objects.equals(backgroundImgUrl, game.backgroundImgUrl) && Objects.equals(verified, game.verified) && Objects.equals(realiseDate, game.realiseDate) && Objects.equals(developers, game.developers) && Objects.equals(publishers, game.publishers) && Objects.equals(genres, game.genres);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + Objects.hashCode(title);
+        result = 31 * result + Objects.hashCode(description);
+        result = 31 * result + Double.hashCode(rating);
+        result = 31 * result + Double.hashCode(avgTime);
+        result = 31 * result + Objects.hashCode(shortDescription);
+        result = 31 * result + Objects.hashCode(imageUrl);
+        result = 31 * result + Objects.hashCode(steamId);
+        result = 31 * result + Objects.hashCode(smallImgUrl);
+        result = 31 * result + Objects.hashCode(backgroundImgUrl);
+        result = 31 * result + Objects.hashCode(verified);
+        result = 31 * result + Objects.hashCode(realiseDate);
+        result = 31 * result + Objects.hashCode(developers);
+        result = 31 * result + Objects.hashCode(publishers);
+        result = 31 * result + Objects.hashCode(genres);
+        return result;
     }
 }
