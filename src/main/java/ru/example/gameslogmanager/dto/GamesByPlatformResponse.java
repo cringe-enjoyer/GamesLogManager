5b1e7d291048589eab820c6ca1754d7a8f01bdde
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-04-18 12:29:43 +0300

    GamesByPlatformResponse store PlatformDTO and UsersGameDTO instead of Platform and UsersGame

--- src/main/java/ru/example/gameslogmanager/dto/GamesByPlatformResponse.java
+++ src/main/java/ru/example/gameslogmanager/dto/GamesByPlatformResponse.java
@@ -1,26 +1,23 @@
 package ru.example.gameslogmanager.dto;
 
-import ru.example.gameslogmanager.models.Platform;
-import ru.example.gameslogmanager.models.UsersGame;
-
 import java.util.List;
 import java.util.Map;
 
 public class GamesByPlatformResponse {
-    private Map<Platform, List<UsersGame>> gamesByPlatform;
+    private Map<PlatformDTO, List<UsersGameDTO>> gamesByPlatform;
 
     public GamesByPlatformResponse() {
     }
 
-    public GamesByPlatformResponse(Map<Platform, List<UsersGame>> gamesByPlatform) {
+    public GamesByPlatformResponse(Map<PlatformDTO, List<UsersGameDTO>> gamesByPlatform) {
         this.gamesByPlatform = gamesByPlatform;
     }
 
-    public Map<Platform, List<UsersGame>> getGamesByPlatform() {
+    public Map<PlatformDTO, List<UsersGameDTO>> getGamesByPlatform() {
         return gamesByPlatform;
     }
 
-    public void setGamesByPlatform(Map<Platform, List<UsersGame>> gamesByPlatform) {
+    public void setGamesByPlatform(Map<PlatformDTO, List<UsersGameDTO>> gamesByPlatform) {
         this.gamesByPlatform = gamesByPlatform;
     }
 }
