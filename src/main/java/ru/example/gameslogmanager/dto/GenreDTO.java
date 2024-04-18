96d47aaab8ba7394ba40c4d14bd8f0c622b09217
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-04-18 12:18:49 +0300

    Remove set of games

--- src/main/java/ru/example/gameslogmanager/dto/GenreDTO.java
+++ src/main/java/ru/example/gameslogmanager/dto/GenreDTO.java
@@ -3,33 +3,29 @@ package ru.example.gameslogmanager.dto;
 import jakarta.validation.constraints.Size;
 
 import java.io.Serializable;
-import java.util.Set;
 
 /**
  * DTO for {@link ru.example.gameslogmanager.models.Genre}
  */
 public class GenreDTO implements Serializable {
     @Size(max = 150)
-    private final String name;
-    private final Set<GameDTO> games;
+    private String name;
 
-    public GenreDTO(String name, Set<GameDTO> games) {
+    public GenreDTO(String name) {
         this.name = name;
-        this.games = games;
     }
 
     public String getName() {
         return name;
     }
 
-    public Set<GameDTO> getGames() {
-        return games;
+    public void setName(String name) {
+        this.name = name;
     }
 
     @Override
     public String toString() {
         return getClass().getSimpleName() + "(" +
-                "name = " + name + ", " +
-                "games = " + games + ")";
+                "name = " + name + ", ";
     }
 }
