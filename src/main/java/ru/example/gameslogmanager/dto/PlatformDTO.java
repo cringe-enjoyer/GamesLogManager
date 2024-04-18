dd0a5e34be6937a0e5670513a3885ac446dfd511
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-04-18 12:25:19 +0300

    Remove set of UsersGame from PlatformDTO

--- src/main/java/ru/example/gameslogmanager/dto/PlatformDTO.java
+++ src/main/java/ru/example/gameslogmanager/dto/PlatformDTO.java
@@ -1,12 +1,9 @@
 package ru.example.gameslogmanager.dto;
 
 import jakarta.validation.constraints.Size;
-import ru.example.gameslogmanager.models.UsersGame;
 
 import java.io.Serializable;
-import java.util.LinkedHashSet;
 import java.util.Objects;
-import java.util.Set;
 
 /**
  * DTO for {@link ru.example.gameslogmanager.models.Platform}
@@ -14,14 +11,12 @@ import java.util.Set;
 public class PlatformDTO implements Serializable {
     @Size(max = 100)
     private String name;
-    private Set<UsersGame> usersGames = new LinkedHashSet<>();
 
     public PlatformDTO() {
     }
 
-    public PlatformDTO(String name, Set<UsersGame> usersGames) {
+    public PlatformDTO(String name) {
         this.name = name;
-        this.usersGames = usersGames;
     }
 
     public String getName() {
@@ -32,32 +27,22 @@ public class PlatformDTO implements Serializable {
         this.name = name;
     }
 
-    public Set<UsersGame> getUsersGames() {
-        return usersGames;
-    }
-
-    public void setUsersGames(Set<UsersGame> usersGames) {
-        this.usersGames = usersGames;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         PlatformDTO entity = (PlatformDTO) o;
-        return Objects.equals(this.name, entity.name) &&
-                Objects.equals(this.usersGames, entity.usersGames);
+        return Objects.equals(this.name, entity.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, usersGames);
+        return Objects.hash(name);
     }
 
     @Override
     public String toString() {
         return getClass().getSimpleName() + "(" +
-                "name = " + name + ", " +
-                "usersGames = " + usersGames + ")";
+                "name = " + name + ", ";
     }
 }
