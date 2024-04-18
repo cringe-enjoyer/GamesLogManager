f536906e7627649d01d93f7988a3cac31668be3b
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-04-18 12:21:03 +0300

    Add FriendDTO when need info about user without sensitive data

---
+++ src/main/java/ru/example/gameslogmanager/dto/FriendDTO.java
@@ -0,0 +1,31 @@
+package ru.example.gameslogmanager.dto;
+
+public class FriendDTO {
+    private String email;
+    private String steamId;
+    private String nickname;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSteamId() {
+        return steamId;
+    }
+
+    public void setSteamId(String steamId) {
+        this.steamId = steamId;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+}
