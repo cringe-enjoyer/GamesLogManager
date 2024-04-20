662f105eaa6e8c722219360833167ac92184ed8f
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-04-20 12:50:39 +0300

    Add null check and user to userId map in GamesListMapper

--- src/main/java/ru/example/gameslogmanager/mapper/GamesListMapper.java
+++ src/main/java/ru/example/gameslogmanager/mapper/GamesListMapper.java
@@ -1,10 +1,13 @@
 package ru.example.gameslogmanager.mapper;
 
+import jakarta.annotation.PostConstruct;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.example.gameslogmanager.dto.GamesListDTO;
 import ru.example.gameslogmanager.models.GamesList;
+
+import java.util.Objects;
 
 @Component
 public class GamesListMapper {
@@ -16,11 +19,19 @@ public class GamesListMapper {
         this.modelMapper = modelMapper;
     }
 
+    @PostConstruct
+    public void setupMapper() {
+        modelMapper.createTypeMap(GamesList.class, GamesListDTO.class).addMappings(mapper -> {
+            mapper.map(src -> src.getUser().getUserId(), GamesListDTO::setUserId);
+            mapper.when(Objects::isNull).skip(GamesList::getUsersGames, GamesListDTO::setUsersGamesList);
+        });
+    }
+
     public GamesList convertToEntity(GamesListDTO gamesListDTO) {
-        return modelMapper.map(gamesListDTO, GamesList.class);
+        return Objects.isNull(gamesListDTO) ? null : modelMapper.map(gamesListDTO, GamesList.class);
     }
 
     public GamesListDTO convertToDTO(GamesList gamesList) {
-        return modelMapper.map(gamesList, GamesListDTO.class);
+        return Objects.isNull(gamesList) ? null : modelMapper.map(gamesList, GamesListDTO.class);
     }
 }
