b64a00d6360cd1b2b4fc05bd8f10602e4285fb34
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-04-20 13:05:51 +0300

    Replace ModelMapper and fix controller methods

--- src/main/java/ru/example/gameslogmanager/controllers/FriendsListController.java
+++ src/main/java/ru/example/gameslogmanager/controllers/FriendsListController.java
@@ -1,12 +1,12 @@
 package ru.example.gameslogmanager.controllers;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.example.gameslogmanager.dto.FriendsListDTO;
+import ru.example.gameslogmanager.mapper.FriendsListMapper;
 import ru.example.gameslogmanager.models.FriendsList;
 import ru.example.gameslogmanager.models.User;
 import ru.example.gameslogmanager.services.FriendsListService;
@@ -14,56 +14,43 @@ import ru.example.gameslogmanager.services.UserService;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/friends/{id}")
+@RequestMapping("/friends")
 public class FriendsListController {
 
     private final FriendsListService friendsListService;
     private final UserService userService;
-    private final ModelMapper modelMapper;
+    private final FriendsListMapper friendsListMapper;
 
     @Autowired
-    public FriendsListController(FriendsListService friendsListService, UserService userService,
-                                 ModelMapper modelMapper) {
+    public FriendsListController(FriendsListService friendsListService, UserService userService, FriendsListMapper friendsListMapper) {
         this.friendsListService = friendsListService;
         this.userService = userService;
-        this.modelMapper = modelMapper;
+        this.friendsListMapper = friendsListMapper;
     }
 
-    @GetMapping
+    @GetMapping("/{id}")
     public List<FriendsListDTO> getFriendsList(@PathVariable("id") int userId) {
         Optional<User> userById = userService.getUserById(userId);
-        if (!userById.isPresent()) {
+        if (userById.isEmpty()) {
             return null;
         }
 
-        return userById.get().getFriendsLists().stream()
-                .map(this::convertToFriendsListDTO)
-                .collect(Collectors.toList());
+        return friendsListService.getFriendsLists(userById.get()).stream()
+                .map(friendsListMapper::convertToDto)
+                .toList();
     }
 
-    @DeleteMapping
-    public HttpEntity<HttpStatus> deleteFriend(@PathVariable("id") int userId,
-                                               @RequestBody FriendsListDTO friendsListDTO) {
-        Optional<User> userById = userService.getUserById(userId);
-        if (userById.isEmpty()) {
+    @DeleteMapping("/{id}")
+    public HttpEntity<HttpStatus> deleteFriend(@PathVariable("id") long id) {
+        Optional<FriendsList> friendsListsById = friendsListService.getFriendsListsById(id);
+        if (friendsListsById.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
-        //TODO: протестировать
-        friendsListService.deleteFriendConnectionByUserAndFriend(userById.get(),
-                convertToFriendsList(friendsListDTO).getFriend());
+        friendsListService.deleteFriendConnectionById(id);
 
         return new HttpEntity<>(HttpStatus.OK);
-    }
-
-    private FriendsList convertToFriendsList(FriendsListDTO friendsListDTO) {
-        return modelMapper.map(friendsListDTO, FriendsList.class);
-    }
-
-    private FriendsListDTO convertToFriendsListDTO(FriendsList element) {
-        return modelMapper.map(element, FriendsListDTO.class);
     }
 }
