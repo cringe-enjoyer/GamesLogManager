a7ed91220662d781091b7dd971ca3fa34487e4df
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-04-20 11:21:25 +0300

    Set strict matching strategy for modelMapper

--- src/main/java/ru/example/gameslogmanager/GamesLogManagerApplication.java
+++ src/main/java/ru/example/gameslogmanager/GamesLogManagerApplication.java
@@ -1,6 +1,7 @@
 package ru.example.gameslogmanager;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +15,8 @@ public class GamesLogManagerApplication {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
     }
 }
