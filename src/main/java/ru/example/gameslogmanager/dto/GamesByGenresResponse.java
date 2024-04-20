446c9e23686ee1eb2dd6c5dc85da63e60c7c42ff
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-04-20 12:48:38 +0300

    Replace entities with DTOs in GamesByGenresResponse

--- src/main/java/ru/example/gameslogmanager/dto/GamesByGenresResponse.java
+++ src/main/java/ru/example/gameslogmanager/dto/GamesByGenresResponse.java
@@ -1,26 +1,23 @@
 package ru.example.gameslogmanager.dto;
 
-import ru.example.gameslogmanager.models.Genre;
-import ru.example.gameslogmanager.models.UsersGame;
-
 import java.util.List;
 import java.util.Map;
 
 public class GamesByGenresResponse {
-    private Map<Genre, List<UsersGame>> gamesByGenres;
+    private Map<GenreDTO, List<UsersGameDTO>> gamesByGenres;
 
     public GamesByGenresResponse() {
     }
 
-    public GamesByGenresResponse(Map<Genre, List<UsersGame>> gamesByGenres) {
+    public GamesByGenresResponse(Map<GenreDTO, List<UsersGameDTO>> gamesByGenres) {
         this.gamesByGenres = gamesByGenres;
     }
 
-    public Map<Genre, List<UsersGame>> getGamesByGenres() {
+    public Map<GenreDTO, List<UsersGameDTO>> getGamesByGenres() {
         return gamesByGenres;
     }
 
-    public void setGamesByGenres(Map<Genre, List<UsersGame>> gamesByGenres) {
+    public void setGamesByGenres(Map<GenreDTO, List<UsersGameDTO>> gamesByGenres) {
         this.gamesByGenres = gamesByGenres;
     }
 }
