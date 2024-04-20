9925a5689a6e7d0e3c6a694c1de7a57e2b669c0d
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-04-20 11:41:01 +0300

    Add component annotation for DevelopersConverter and PublishersConverter

--- src/main/java/ru/example/gameslogmanager/mapper/DevelopersConverter.java
+++ src/main/java/ru/example/gameslogmanager/mapper/DevelopersConverter.java
@@ -1,11 +1,13 @@
 package ru.example.gameslogmanager.mapper;
 
 import org.modelmapper.AbstractConverter;
+import org.springframework.stereotype.Component;
 import ru.example.gameslogmanager.models.Developer;
 
 import java.util.Set;
 import java.util.stream.Collectors;
 
+@Component
 public class DevelopersConverter extends AbstractConverter<Set<Developer>, Set<Integer>> {
 
     @Override
