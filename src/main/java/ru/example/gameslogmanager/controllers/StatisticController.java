fc2034ee6f59198ae144a616bc5cab88374b26f1
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-04-20 13:10:03 +0300

    Replace ModelMapper and fix methods in StatisticController

--- src/main/java/ru/example/gameslogmanager/controllers/StatisticController.java
+++ src/main/java/ru/example/gameslogmanager/controllers/StatisticController.java
@@ -2,19 +2,17 @@ package ru.example.gameslogmanager.controllers;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import ru.example.gameslogmanager.dto.GamesListDTO;
-import ru.example.gameslogmanager.dto.StatisticResponse;
-import ru.example.gameslogmanager.dto.UserDTO;
-import ru.example.gameslogmanager.dto.UsersGoalDTO;
-import ru.example.gameslogmanager.models.User;
-import ru.example.gameslogmanager.models.UsersGoal;
+import org.springframework.web.bind.annotation.*;
+import ru.example.gameslogmanager.dto.*;
+import ru.example.gameslogmanager.mapper.*;
+import ru.example.gameslogmanager.models.*;
 import ru.example.gameslogmanager.services.StatisticService;
+import ru.example.gameslogmanager.services.UserService;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -22,19 +20,32 @@ import java.util.stream.Collectors;
 public class StatisticController {
 
     private final StatisticService statisticService;
-    private final ModelMapper modelMapper;
+    private final UserService userService;
+    private final PlatformMapper platformMapper;
+    private final UsersGameMapper usersGameMapper;
+    private final UsersGoalMapper usersGoalMapper;
+    private final GenreMapper genreMapper;
 
     @Autowired
-    public StatisticController(StatisticService statisticService, ModelMapper modelMapper) {
+    public StatisticController(StatisticService statisticService, UserService userService,
+                               PlatformMapper platformMapper, UsersGameMapper usersGameMapper,
+                               UsersGoalMapper usersGoalMapper, GenreMapper genreMapper) {
         this.statisticService = statisticService;
-        this.modelMapper = modelMapper;
+        this.userService = userService;
+        this.platformMapper = platformMapper;
+        this.usersGameMapper = usersGameMapper;
+        this.usersGoalMapper = usersGoalMapper;
+        this.genreMapper = genreMapper;
     }
 
     //TODO: как бы правильно возвращать эти данные?
     @GetMapping()
-    public StatisticResponse getStats(UserDTO userDTO) {
-        User user = convertToUser(userDTO);
-        Map<String, Object> userStats = statisticService.getUserStats(user);
+    public StatisticResponse getStats(@RequestParam int userId) {
+        Optional<User> user = userService.getUserById(userId);
+        if (user.isEmpty())
+            return null;
+
+        Map<String, Object> userStats = statisticService.getUserStats(user.get());
 
         StatisticResponse response = new StatisticResponse();
         
@@ -44,13 +55,45 @@ public class StatisticController {
 
         List<UsersGoal> goals = (List<UsersGoal>) userStats.get("user-goal");
         response.setGoal(goals == null ? null : goals.stream()
-                .map((element) -> modelMapper.map(element, UsersGoalDTO.class))
+                .map(usersGoalMapper::convertToDTO)
                 .collect(Collectors.toList()));
 
         return response;
     }
 
-    private User convertToUser(UserDTO userDTO) {
-        return modelMapper.map(userDTO, User.class);
+    @GetMapping("/{id}/platform")
+    public GamesByPlatformResponse getPlatformStats(@PathVariable("id") Integer userId) {
+        Optional<User> user = userService.getUserById(userId);
+        if (user.isEmpty()) {
+            return null;
+        }
+
+        Map<Platform, List<UsersGame>> userStats = statisticService.getGamesByPlatforms(user.get());
+        Map<PlatformDTO, List<UsersGameDTO>> platformStats = new HashMap<>();
+        userStats.forEach((platform, usersGames) ->
+                platformStats.put(platformMapper.convertToDTO(platform), usersGames.stream()
+                        .map(usersGameMapper::convertToDTO)
+                        .collect(Collectors.toList())));
+        return new GamesByPlatformResponse(platformStats);
+    }
+
+    @GetMapping("/{id}/genres")
+    public GamesByGenresResponse getGenresStats(@PathVariable("id") Integer userId) {
+        Optional<User> user = userService.getUserById(userId);
+        if (user.isEmpty()) {
+            return null;
+        }
+
+        Map<Genre, List<UsersGame>> userStats = statisticService.getGamesByGenres(user.get());
+
+        if (userStats.isEmpty()) return null;
+
+        Map<GenreDTO, List<UsersGameDTO>> genreStats = new HashMap<>();
+        userStats.forEach((genre, usersGames) ->
+            genreStats.put(genreMapper.convertToDTO(genre),
+                    usersGames.stream().map(usersGameMapper::convertToDTO).collect(Collectors.toList())));
+
+
+        return new GamesByGenresResponse(genreStats);
     }
 }
