581d62683b0a7d3dbfb4375e72ada4766375a73a
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-04-20 12:55:59 +0300

    Add null check and method that update exist entity with DTO

--- src/main/java/ru/example/gameslogmanager/mapper/PlatformMapper.java
+++ src/main/java/ru/example/gameslogmanager/mapper/PlatformMapper.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Component;
 import ru.example.gameslogmanager.dto.PlatformDTO;
 import ru.example.gameslogmanager.models.Platform;
 
+import java.util.Objects;
+
 @Component
 public class PlatformMapper {
 
@@ -16,11 +18,26 @@ public class PlatformMapper {
         this.modelMapper = modelMapper;
     }
 
+    /**
+     * Обновляет поля в существующей сущности на основе DTO
+     *
+     * @param platformDTO DTO с новыми данными
+     * @param platform существующая сущность платформы
+     * @return сущность платформы с обновленными данными
+     */
+    public Platform convertToEntity(PlatformDTO platformDTO, Platform platform) {
+        if (Objects.isNull(platformDTO))
+            return null;
+
+        modelMapper.map(platformDTO, platform);
+        return platform;
+    }
+
     public Platform convertToEntity(PlatformDTO platformDTO) {
-        return modelMapper.map(platformDTO, Platform.class);
+        return Objects.isNull(platformDTO) ? null : modelMapper.map(platformDTO, Platform.class);
     }
 
     public PlatformDTO convertToDTO(Platform platform) {
-        return modelMapper.map(platform, PlatformDTO.class);
+        return Objects.isNull(platform) ? null : modelMapper.map(platform, PlatformDTO.class);
     }
 }
