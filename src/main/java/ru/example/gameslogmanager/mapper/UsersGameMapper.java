37827838469614b8332cb6d0f973b563bace1bee
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-04-20 12:44:51 +0300

    Add convert to entity logic for UsersGameMapper. Use id fields to get entities by their ids

--- src/main/java/ru/example/gameslogmanager/mapper/UsersGameMapper.java
+++ src/main/java/ru/example/gameslogmanager/mapper/UsersGameMapper.java
@@ -5,18 +5,31 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.example.gameslogmanager.dto.UsersGameDTO;
+import ru.example.gameslogmanager.models.Game;
+import ru.example.gameslogmanager.models.GamesList;
+import ru.example.gameslogmanager.models.Platform;
 import ru.example.gameslogmanager.models.UsersGame;
+import ru.example.gameslogmanager.services.GameService;
+import ru.example.gameslogmanager.services.GamesListService;
+import ru.example.gameslogmanager.services.PlatformService;
 
 import java.util.Objects;
+import java.util.Optional;
 
 @Component
 public class UsersGameMapper {
 
     private final ModelMapper modelMapper;
+    private final GameService gameService;
+    private final GamesListService gamesListService;
+    private final PlatformService platformService;
 
     @Autowired
-    public UsersGameMapper(ModelMapper modelMapper) {
+    public UsersGameMapper(ModelMapper modelMapper, GameService gameService, GamesListService gamesListService, PlatformService platformService) {
         this.modelMapper = modelMapper;
+        this.gameService = gameService;
+        this.gamesListService = gamesListService;
+        this.platformService = platformService;
     }
 
     @PostConstruct
@@ -27,7 +40,28 @@ public class UsersGameMapper {
     }
 
     public UsersGame convertToEntity(UsersGameDTO usersGameDTO) {
-        return Objects.isNull(usersGameDTO) ? null : modelMapper.map(usersGameDTO, UsersGame.class);
+        if (Objects.isNull(usersGameDTO))
+            return null;
+
+        UsersGame usersGame = modelMapper.map(usersGameDTO, UsersGame.class);
+        Optional<Game> gameById = gameService.getGameById(usersGameDTO.getGameId());
+        Optional<GamesList> list = gamesListService.getById(usersGameDTO.getListId());
+        Optional<Platform> platform = platformService.getByName(usersGameDTO.getPlatform().getName());
+
+        usersGame.setGame(gameById.orElse(null));
+        usersGame.setList(list.orElse(null));
+        usersGame.setPlatform(platform.orElse(null));
+
+        return usersGame;
+    }
+
+    public UsersGame convertToEntity(UsersGameDTO usersGameDTO, UsersGame usersGame) {
+        if (Objects.isNull(usersGameDTO))
+            return null;
+
+        modelMapper.map(usersGameDTO, usersGame);
+
+        return usersGame;
     }
 
     public UsersGameDTO convertToDTO(UsersGame usersGame) {
