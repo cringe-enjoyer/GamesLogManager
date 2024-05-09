d59f2c3e8a02ae0647d18202eeec062599e0afda
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-05-09 11:39:09 +0300

    Added exception api handler

---
+++ src/main/java/ru/example/gameslogmanager/controllers/ExceptionApiHandler.java
@@ -0,0 +1,50 @@
+package ru.example.gameslogmanager.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.example.gameslogmanager.dto.ErrorResponse;
+import ru.example.gameslogmanager.exceptions.*;
+
+@RestControllerAdvice
+public class ExceptionApiHandler {
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleException(UserNotFoundException e) {
+        ErrorResponse response = new ErrorResponse(e.getMessage());
+
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(FriendsListNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleException(FriendsListNotFoundException e) {
+        ErrorResponse response = new ErrorResponse(e.getMessage());
+
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(GamesListNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleException(GamesListNotFoundException e) {
+        ErrorResponse response = new ErrorResponse(e.getMessage());
+        
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(GameNotCreated.class)
+    public ResponseEntity<ErrorResponse> handleException(GameNotCreated e) {
+        ErrorResponse response = new ErrorResponse(e.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(DeveloperNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleException(DeveloperNotFoundException e) {
+        ErrorResponse response = new ErrorResponse(e.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(UsersGameNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleException(UsersGameNotFoundException e) {
+        ErrorResponse response = new ErrorResponse(e.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+}
