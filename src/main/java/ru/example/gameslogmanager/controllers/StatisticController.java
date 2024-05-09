8b5a597a7307f370e7ab08d20100e71bcdcc1f13
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-05-09 13:02:25 +0300

    Added methods with statistic for users games

--- src/main/java/ru/example/gameslogmanager/controllers/StatisticController.java
+++ src/main/java/ru/example/gameslogmanager/controllers/StatisticController.java
@@ -1,14 +1,20 @@
 package ru.example.gameslogmanager.controllers;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.example.gameslogmanager.dto.*;
-import ru.example.gameslogmanager.mapper.*;
+import ru.example.gameslogmanager.exceptions.UserNotFoundException;
+import ru.example.gameslogmanager.mapper.GameMapper;
+import ru.example.gameslogmanager.mapper.UsersGoalMapper;
 import ru.example.gameslogmanager.models.*;
+import ru.example.gameslogmanager.services.GameService;
 import ru.example.gameslogmanager.services.StatisticService;
 import ru.example.gameslogmanager.services.UserService;
+import ru.example.gameslogmanager.dto.GamesPerMonthResponse;
+import ru.example.gameslogmanager.services.UsersGameService;
+import ru.example.gameslogmanager.utils.DefaultLists;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,21 +27,21 @@ public class StatisticController {
 
     private final StatisticService statisticService;
     private final UserService userService;
-    private final PlatformMapper platformMapper;
-    private final UsersGameMapper usersGameMapper;
     private final UsersGoalMapper usersGoalMapper;
-    private final GenreMapper genreMapper;
+    private final GameService gameService;
+    private final GameMapper gameMapper;
+    private final UsersGameService usersGameService;
 
     @Autowired
     public StatisticController(StatisticService statisticService, UserService userService,
-                               PlatformMapper platformMapper, UsersGameMapper usersGameMapper,
-                               UsersGoalMapper usersGoalMapper, GenreMapper genreMapper) {
+                               UsersGoalMapper usersGoalMapper, GameService gameService,
+                               GameMapper gameMapper, UsersGameService usersGameService) {
         this.statisticService = statisticService;
         this.userService = userService;
-        this.platformMapper = platformMapper;
-        this.usersGameMapper = usersGameMapper;
         this.usersGoalMapper = usersGoalMapper;
-        this.genreMapper = genreMapper;
+        this.gameService = gameService;
+        this.gameMapper = gameMapper;
+        this.usersGameService = usersGameService;
     }
 
     //TODO: как бы правильно возвращать эти данные?
@@ -62,31 +68,38 @@ public class StatisticController {
     }
 
     @GetMapping("/{id}/platform")
-    public GamesByPlatformResponse getPlatformStats(@PathVariable("id") Integer userId) {
+    public Map<String, Long> getPlatformStats(@PathVariable("id") Integer userId) {
         Optional<User> user = userService.getUserById(userId);
         if (user.isEmpty()) {
-            return null;
+            String message = "User not found";
+            throw new UserNotFoundException(message);
         }
 
-        Map<Platform, List<UsersGame>> userStats = statisticService.getGamesByPlatforms(user.get());
-        Map<PlatformDTO, List<UsersGameDTO>> platformStats = new HashMap<>();
+/*        Map<Platform, List<UsersGame>> userStats = statisticService.getGamesByPlatforms(user.get());
+        Map<String, List<UsersGameDTO>> platformStats = new HashMap<>();
         userStats.forEach((platform, usersGames) ->
-                platformStats.put(platformMapper.convertToDTO(platform), usersGames.stream()
+                platformStats.put(platform.getName(), usersGames.stream()
                         .map(usersGameMapper::convertToDTO)
                         .collect(Collectors.toList())));
-        return new GamesByPlatformResponse(platformStats);
+        return new GamesByPlatformResponse(platformStats);*/
+
+        Map<Platform, Long> userStats = statisticService.getGamesCountByPlatforms(user.get());
+        Map<String, Long> platformStats = new HashMap<>();
+        userStats.forEach((platform, count) -> platformStats.put(platform.getName(), count));
+        return platformStats;
     }
 
     @GetMapping("/{id}/genres")
-    public GamesByGenresResponse getGenresStats(@PathVariable("id") Integer userId) {
+    public Map<String, Long> getGenresStats(@PathVariable("id") Integer userId) {
         Optional<User> user = userService.getUserById(userId);
         if (user.isEmpty()) {
-            return null;
+            String message = "User not found";
+            throw new UserNotFoundException(message);
         }
-
+/*
         Map<Genre, List<UsersGame>> userStats = statisticService.getGamesByGenres(user.get());
 
-        if (userStats.isEmpty()) return null;
+        if (userStats.isEmpty()) return Map.of();
 
         Map<GenreDTO, List<UsersGameDTO>> genreStats = new HashMap<>();
         userStats.forEach((genre, usersGames) ->
@@ -94,6 +107,49 @@ public class StatisticController {
                     usersGames.stream().map(usersGameMapper::convertToDTO).collect(Collectors.toList())));
 
 
-        return new GamesByGenresResponse(genreStats);
+        return new GamesByGenresResponse(genreStats);*/
+
+        return statisticService.getCountGamesByGenres(user.get());
+    }
+
+    @GetMapping("/{id}/month")
+    public List<GamesPerMonthResponse> getGamesPerMonth(@PathVariable("id") int userId,
+                                                        @RequestParam(required = false) Integer year) {
+        Optional<User> user = userService.getUserById(userId);
+        if (user.isEmpty()) {
+            String message = "User not found";
+            throw new UserNotFoundException(message);
+        }
+        if (year == null || year == 0)
+            year = LocalDate.now().getYear();
+
+        return statisticService.getFinishedGamesPerMonth(user.get(), year);
+    }
+
+    @GetMapping("/{id}/in-progress")
+    public List<GameDTO> getCurrentGames(@PathVariable("id") int userId) {
+        Optional<User> user = userService.getUserById(userId);
+        if (user.isEmpty()) {
+            String message = "User not found";
+            throw new UserNotFoundException(message);
+        }
+
+        return gameService.getGamesInList(user.get(), DefaultLists.INPROGRES.getRuValue()).stream()
+                .map(gameMapper::convertToDTO)
+                .toList();
+    }
+
+    @GetMapping("/{id}/last-finished")
+    public List<GameDTO> getLastFinishedGames(@PathVariable("id") int userId) {
+        Optional<User> user = userService.getUserById(userId);
+        if (user.isEmpty()) {
+            String message = "User not found";
+            throw new UserNotFoundException(message);
+        }
+
+        return usersGameService.get5LastFinishedGames(user.get()).stream()
+                .map(UsersGame::getGame)
+                .map(gameMapper::convertToDTO)
+                .toList();
     }
 }
