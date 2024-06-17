14cd4932fc21cbc76d536117f5b29151be13236f
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2024-06-17 13:38:10 +0300

    Fix saveUserGameData and deleteUsersGame, and add getUserGameByGameAndUser method

--- src/main/java/ru/example/gameslogmanager/controllers/UsersGameController.java
+++ src/main/java/ru/example/gameslogmanager/controllers/UsersGameController.java
@@ -5,9 +5,12 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.example.gameslogmanager.dto.UsersGameDTO;
+import ru.example.gameslogmanager.exceptions.GameNotFoundException;
+import ru.example.gameslogmanager.exceptions.UserNotFoundException;
 import ru.example.gameslogmanager.exceptions.UsersGameNotFoundException;
 import ru.example.gameslogmanager.models.Game;
 import ru.example.gameslogmanager.models.GamesList;
+import ru.example.gameslogmanager.models.User;
 import ru.example.gameslogmanager.models.UsersGame;
 import ru.example.gameslogmanager.services.*;
 import ru.example.gameslogmanager.mapper.UsersGameMapper;
@@ -23,15 +26,18 @@ public class UsersGameController {
     private final GameService gameService;
     private final GamesListService gamesListService;
     private final SteamService steamService;
+    private final UserService userService;
 
     @Autowired
     public UsersGameController(UsersGameService usersGameService, UsersGameMapper usersGameMapper,
-                               GameService gameService, GamesListService gamesListService, SteamService steamService) {
+                               GameService gameService, GamesListService gamesListService, SteamService steamService,
+                               UserService userService) {
         this.usersGameService = usersGameService;
         this.usersGameMapper = usersGameMapper;
         this.gameService = gameService;
         this.gamesListService = gamesListService;
         this.steamService = steamService;
+        this.userService = userService;
     }
 
     @GetMapping()
@@ -57,6 +63,34 @@ public class UsersGameController {
         return response;
     }
 
+    @GetMapping("/user")
+    public UsersGameDTO getUserGameByGameAndUser(@RequestParam Integer gameId, @RequestParam Integer userId) {
+        Optional<User> user = userService.getUserById(userId);
+        if (user.isEmpty()) {
+            String message = "User with id not found";
+            throw new UserNotFoundException(message);
+        }
+
+        Optional<Game> game = gameService.getGameById(gameId);
+
+        if (game.isEmpty()) {
+            String message = "Game with not found";
+            throw new GameNotFoundException(message);
+        }
+
+        Optional<UsersGame> userGame = usersGameService.getUsersGameByGameAndUser(game.get(), user.get());
+
+        UsersGameDTO response = null;
+        if (userGame.isPresent()) {
+            response = usersGameMapper.convertToDTO(userGame.get());
+
+            if (game.get().getSteamId() != 0 && userGame.get().getFromSteam())
+                response.setAchievements(steamService.getUserAchievementsForGame(game.get(), user.get()));
+        }
+
+        return response;
+    }
+
     @GetMapping("/{id}")
     public UsersGameDTO getUserGameById(@PathVariable("id") Integer userGameId) {
         Optional<UsersGame> game = usersGameService.getUsersGameById(userGameId);
@@ -74,65 +108,60 @@ public class UsersGameController {
         return response;
     }
 
-/*    @GetMapping()
-    public UsersGameDTO getUserGameByGameAndUser(@RequestParam Integer gameId, @RequestParam Integer userId) {
-        Optional<User> user = userService.getUserById(userId);
-        if (user.isEmpty()) {
-            String message = "User with id not found";
-            throw new UserNotFoundException(message);
-        }
-
-        Optional<Game> game = gameService.getGameById(gameId);
-
-        if (game.isEmpty()) {
-            String message = "Game with not found";
-            throw new GameNotFoundException(message);
-        }
-
-        Optional<UsersGame> usersGame = usersGameService.getUsersGameByGameAndUser(game.get(), user.get());
-
-        if (usersGame.isEmpty()) {
-            String message = "Users game not found";
-            throw new UsersGameNotFoundException(message);
-        }
-
-        UsersGameDTO response = usersGameMapper.convertToDTO(usersGame.get());
-
-        if (game.get().getSteamId() != null && usersGame.get().getFromSteam())
-            response.setAchievements(steamService.getUserAchievementsForGame(game.get(), user.get()));
-
-        return response;
-    }*/
-
     @DeleteMapping("/{id}")
     public HttpEntity<HttpStatus> deleteUsersGame(@PathVariable("id") int usersGameId) {
         Optional<UsersGame> usersGame = usersGameService.getUsersGameById(usersGameId);
-        if (usersGame.isEmpty())
-            return new HttpEntity<>(HttpStatus.BAD_REQUEST);
+        if (usersGame.isEmpty()) {
+            String message = "Users game with id " + usersGameId + " not found";
+            throw new UsersGameNotFoundException(message);
+        }
 
         usersGameService.remove(usersGame.get());
         return new HttpEntity<>(HttpStatus.OK);
     }
 
-    @PostMapping("/add")
-    public HttpEntity<HttpStatus> saveUserGameData(@RequestBody UsersGameDTO gameDTO) {
-        Optional<Game> game = gameService.getGameById(gameDTO.getGameId());
-        Optional<GamesList> list = gamesListService.getById(gameDTO.getListId());
+    @DeleteMapping()
+    public HttpEntity<HttpStatus> deleteUsersGame(@RequestParam("userId") int userId,
+                                                  @RequestParam("gameId") int gameId) {
+        Optional<Game> game = gameService.getGameById(gameId);
+        if (game.isEmpty()) {
+            String message = "Game with id " + gameId + " not found";
+            throw new GameNotFoundException(message);
+        }
+
+        Optional<User> user = userService.getUserById(userId);
+        if (user.isEmpty()) {
+            String message = "User with id " + userId + " not found";
+            throw new UserNotFoundException(message);
+        }
+
+        Optional<UsersGame> usersGame = usersGameService.getUsersGameByGameAndUser(game.get(), user.get());
+        if (usersGame.isEmpty()) {
+            String message = "Users game not found";
+            throw new UsersGameNotFoundException(message);
+        }
+
+        usersGameService.remove(usersGame.get());
+        return new HttpEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/add/{id}")
+    public HttpEntity<HttpStatus> saveUserGameData(@RequestBody UsersGameDTO usersGameDTO, @PathVariable int id) {
+        System.out.println(usersGameDTO);
+        Optional<Game> game = gameService.getGameById(usersGameDTO.getGameId());
+        Optional<GamesList> list = gamesListService.getById(usersGameDTO.getListId());
         UsersGame usersGame = null;
         if (game.isPresent() && list.isPresent()) {
             Optional<UsersGame> usersGameInBD = usersGameService.getUsersGameByGameAndList(game.get(), list.get());
             if (usersGameInBD.isPresent()) {
-                usersGame = usersGameMapper.convertToEntity(gameDTO, usersGameInBD.get());
+                usersGame = usersGameMapper.convertToEntity(usersGameDTO, usersGameInBD.get());
 
                 usersGameService.save(usersGame);
                 return new HttpEntity<>(HttpStatus.OK);
             }
         }
 
-        usersGame = usersGameMapper.convertToEntity(gameDTO);
-/*        usersGame.setPlatform(platformService.getByName(gameDTO.getPlatform().getName()).orElse(null));
-        usersGame.setGame(gameService.getGameById(gameDTO.getGameId()).orElseThrow());
-        usersGame.setList(gamesListService.getById(gameDTO.getListId()).orElseThrow());*/
+        usersGame = usersGameMapper.convertToEntity(usersGameDTO);
 
         usersGameService.save(usersGame);
         return new HttpEntity<>(HttpStatus.OK);
@@ -168,9 +197,7 @@ public class UsersGameController {
             return null;
 
         usersGameService.synchronizeTimeWithSteam(usersGameId, game.get().getSteamId());
-        /*long time = steamService.getUserTimeInGame(game.get().getSteamId(), usersGameDTO.getList().getUser().getSteamId());
-        usersGameDTO.setUserTime(time);
-        usersGameService.save(convertToUsersGame(usersGameDTO));*/
+
         return new HttpEntity<>(HttpStatus.OK);
     }
 }
